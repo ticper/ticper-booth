43af7cc6f7d5ef7d6648145fc43b84f2912558ed
Ryo Nakagome <nakagome@tento-net.com>
2017-12-17 19:13:16 +0900

    初期ページ作ったよ Close #1

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php﻿
+
+?>
+<!DOCTYPE HTML>
+<html>
+  <head>
+    <meta charset="UTF-8">
+
+    <!-- Materialize -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-alpha.2/css/materialize.min.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-alpha.2/js/materialize.min.js"></script>
+
+    <title>ログイン - Ticper</title>
+  </head>
+  <body>
+
+  </body>
+</html>
