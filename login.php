fff3a5b8ddeba3e7728a01078ebe89b558a3b2d2
Ryo Nakagome <nakagome@tento-net.com>
2018-01-26 16:01:16 +0900

    ログイン処理のXSS対策

--- login.php
+++ login.php
@@ -4,14 +4,19 @@
 	
 	$user_id = $_POST['user_id'];
 	$password = $_POST['password'];
+	mysqli_real_escape_string($user_id);
+	mysqli_real_escape_string($password);
+	if(!empty($user_id) AND !empty($password))
+		$sql = mysqli_query($link, "SELECT * FROM user_booth WHERE userid = '$user_id';");
+		$result = mysqli_fetch_assoc($sql);
 
-	$sql = mysqli_query($link, "SELECT * FROM user_booth WHERE userid = '$user_id';");
-	$result = mysqli_fetch_assoc($sql);
-
-	if ($result['userid'] == $user_id AND $result['password'] == $password) {
-		$_SESSION['user_id'] = $user_id;
-		print('<script>location.href="home.php";</script>');
+		if ($result['userid'] == $user_id AND $result['password'] == $password) {
+			$_SESSION['user_id'] = $user_id;
+			print('<script>location.href="home.php";</script>');
+		} else {
+			print('<script>alert("入力内容が間違っています。"); location.href="index.php";</script>');
+		}
 	} else {
-		print('<script>alert("入力内容が間違っています。"); location.href="index.php";</script>');
+		print("<script>alert('クリスサイトスクリプティングの可能性があるため、初期ページに転移します。'; location.href='index.php';</script>")
 	}
 ?>
