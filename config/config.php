c527f3761edd0c770e59335d69f52c15e03dca24
Ryo Nakagome <nakagome@tento-net.com>
2018-01-26 23:03:19 +0900

    コンフィグのbody部分を削除

--- config/config.php
+++ config/config.php
@@ -7,15 +7,3 @@
 	$link = mysqli_connect($db_host, $db_user, $db_pswd, $db_name);
 	mysqli_set_charset($link, "utf8");
 ?>
-
-<!DOCTYPE HTML>
-<html>
-	<head>
-		<meta charset="UTF-8">
-		<title>Config</title>
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	</head>
-	<body>
-		<p>Can't open config file.</p>
-	</body>
-</html>
