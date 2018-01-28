649cf05e91a2291e1fcc3dfa0096557228baf47d
Ryo Nakagome <nakagome@tento-net.com>
2018-01-28 16:05:22 +0900

    Configデータ変更

--- config/config.php
+++ config/config.php
@@ -1,15 +1,10 @@
 <?php
-<<<<<<< HEAD
+
 	$db_host = "118.241.134.12";
 	$db_user = "ticper2";
 	$db_pswd = "ticp-37648";
 	$db_name = "ticper2";
-=======
-	$db_host = "localhost";
-	$db_user = "root";
-	$db_pswd = "";
-	$db_name = "ticper";
->>>>>>> 60d951620c47d4206d0c654c979d29bb9368d7ea
+
 
 	$link = mysqli_connect($db_host, $db_user, $db_pswd, $db_name);
 	mysqli_set_charset($link, "utf8");
