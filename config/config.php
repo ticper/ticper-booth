f2ae63f5eeec9d8a3669a5a209c9f04f6eb555db
Ryo Nakagome <nakagome@tento-net.com>
2018-01-28 16:01:24 +0900

    統一DB鯖用Configファイルに変更

--- config/config.php
+++ config/config.php
@@ -1,8 +1,8 @@
 <?php
-	$db_host = "localhost";
-	$db_user = "root";
+	$db_host = "118.241.134.12";
+	$db_user = "ticper2";
 	$db_pswd = "ticp-37648";
-	$db_name = "ticper";
+	$db_name = "ticper2";
 
 	$link = mysqli_connect($db_host, $db_user, $db_pswd, $db_name);
 	mysqli_set_charset($link, "utf8");
