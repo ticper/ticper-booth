a42268a49d2a268a89eac758bca4d22f3c9821cf
Ryo Nakagome <nakagome@tento-net.com>
2018-01-28 16:08:41 +0900

    Config変更（3回目）

--- config/config.php
+++ config/config.php
@@ -1,8 +1,8 @@
 <?php
-	$db_host = "localhost";
-	$db_user = "root";
-	$db_pswd = "";
-	$db_name = "ticper";
+	$db_host = "db.ticper.com";
+	$db_user = "ticper2";
+	$db_pswd = "ticp-37648";
+	$db_name = "ticper2";
 
 	$link = mysqli_connect($db_host, $db_user, $db_pswd, $db_name);
 	mysqli_set_charset($link, "utf8");
